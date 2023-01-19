db08d2d59a071190a9daef178077bda12fe8dbc6
CYBER NETICS <cybernetics.me@outlook.com>
2023-01-19 12:44:30 +0530

    small updates

--- resources/views/location-map-clinic.blade.php
+++ resources/views/location-map-clinic.blade.php
@@ -5,12 +5,12 @@
         <div class="row">
             <div class="col-12 mt-5 mb-5">
                 <h3 class="text-center text-primary">Clinic Location</h3>
-                <input type="hidden" id="doc_latitude" value="{{ $clinic->con_latitude }}">
-                <input type="hidden" id="doc_longitude" value="{{ $clinic->con_longitude }}">
+                <input type="hidden" id="doc_latitude" value="{{ $clinic->latitude }}">
+                <input type="hidden" id="doc_longitude" value="{{ $clinic->longitude }}">
                 <div id="doclocationmap"></div>
             </div>
             <div class="col text-center">
-                <a href="https://maps.google.com/maps?daddr={{ $clinic->con_latitude }},{{ $clinic->con_longitude }}&11=" class="btn btn-outline btn-light bg-hover-light text-dark text-hover-primary border-color-grey border-color-active-primary border-color-hover-primary text-uppercase rounded-0 px-4 py-2 mb-4 text-2">OPEN IN MAP</a>
+                <a href="https://maps.google.com/maps?daddr={{ $clinic->latitude }},{{ $clinic->longitude }}&11=" class="btn btn-outline btn-light bg-hover-light text-dark text-hover-primary border-color-grey border-color-active-primary border-color-hover-primary text-uppercase rounded-0 px-4 py-2 mb-4 text-2">OPEN IN MAP</a>
             </div>
         </div>
     </div>
