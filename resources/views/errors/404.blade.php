c7b1758bad0e400d8503d8a64a5f80ec22081925
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-07 09:30:54 +0530

    small updates

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,4 @@
-@extends('errors::minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@extends('templates.base')
+@section("content")
+    <h5 class="text-danger">{{ $exception->getMessage() }}</h5>
+@endsection
