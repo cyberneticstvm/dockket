d06647ed3b1da4a8191e7319ddb74f7b5431af5a
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-17 11:14:12 +0530

    small updates

---
+++ database/migrations/2023_02_17_110904_alter_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('address')->after('mobile')->nullable();
+            $table->double('latitude')->after('address')->nullable();
+            $table->double('longitude')->after('latitude')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('address');
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
+        });
+    }
+};
